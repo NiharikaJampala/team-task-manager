2a6a9bb16ce9b1bd961bc40dd58cb8f90468ee68
NiharikaJampala <niharikajampala15@gmail.com>
2026-05-01 22:36:22 +0530

    added os import

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, flash
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.secret_key = 'secret123'
